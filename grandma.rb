07f4fd5ac1e7ab46f3bc75c7bcaacd5d475f1314
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-17 19:48:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if speak_to_grandma !== string.upcase
+  if speak_to_grandma != string.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif speak_to_grandma == string.upcase
     puts "NO, NOT SINCE 1938!"
